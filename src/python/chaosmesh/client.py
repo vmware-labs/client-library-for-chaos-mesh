ab6a2f5787b03ba33dd092bdd5d2eb86769647df
gvishrant <gvishrant@vmware.com>
2023-05-01 12:34:32 -0500

    [skip ci] Minor improvements

--- src/python/chaosmesh/client.py
+++ src/python/chaosmesh/client.py
@@ -15,17 +15,23 @@ class Client:
         factory (ExperimentFactory): An instance of `ExperimentFactory` class to get the experiment instance.
 
     Methods:
-        start_experiment (Experiment, str, str, **kwargs) -> None:
+        start_experiment (experiment_type: Experiment, namespace: str, name: str, **kwargs) -> dict:
             Starts the specified chaos experiment.
 
-        pause_experiment (Experiment, str, str, **kwargs) -> None:
+        pause_experiment (experiment_type: Experiment, namespace: str, name: str, **kwargs) -> dict:
             Pauses the specified chaos experiment.
 
-        delete_experiment (Experiment, str, str, **kwargs) -> None:
+        delete_experiment (experiment_type: Experiment, namespace: str, name: str, **kwargs) -> dict:
             Deletes the specified chaos experiment.
+
+        schedule_experiment (experiment_type: Experiment, namespace: str, name: str, cron_schedule: str, **kwargs) -> dict:
+            Schedules the specified chaos experiment to run on a cron schedule.
+
+        delete_schedule (experiment_type: Experiment, namespace: str, name: str, **kwargs) -> dict:
+            Deletes the schedule for the specified chaos experiment.
     """
 
-    def __init__(self, version):
+    def __init__(self, version: str):
         """
         The constructor for the `Client` class.
 
@@ -83,8 +89,34 @@ class Client:
         return self.factory.get_experiment(experiment_type, **kwargs).delete(namespace=namespace, name=name)
 
     def schedule_experiment(self, experiment_type: Experiment, namespace: str, name: str, cron_schedule: str, **kwargs):
+        """
+        Creates a schedule for the specified chaos experiment.
+
+        Arguments:
+            experiment_type (Experiment): The type of chaos experiment to schedule.
+            namespace (str): The namespace in which the experiment is to be run.
+            name (str): The name of the chaos experiment.
+            cron_schedule (str): The cron schedule in which the experiment should run.
+            **kwargs: The optional parameters for the experiment.
+
+        Returns:
+            dict: The created schedule's resource in Kubernetes.
+        """
         log.info(f"scheduling {experiment_type.value} experiment {name} in {namespace} namespace")
         return Schedule(experiment=self.factory.get_experiment(experiment_type, **kwargs), schedule=cron_schedule, **kwargs).submit(namespace=namespace, name=name)
 
     def delete_schedule(self, experiment_type: Experiment, namespace: str, name: str, **kwargs):
+        """
+        Deletes the schedule for the specified chaos experiment.
+
+        Arguments:
+            experiment_type (Experiment): The type of chaos experiment to unschedule.
+            namespace (str): The namespace in which the experiment is running.
+            name (str): The name of the chaos experiment.
+            **kwargs: The optional parameters for the experiment.
+
+        Returns:
+            None
+        """
+        log.info(f"deleting schedule for {experiment_type.value} experiment {name} in {namespace} namespace")
         return Schedule(self.factory.get_experiment(experiment_type, **kwargs)).delete(namespace=namespace, name=name)
